578625a40dccd5549efb7a65aa9ebd8f00dd75f7
George Scheer <gjscheer@ucdavis.edu>
2018-11-28 23:39:25 -0800

    delay solar cleanup by a week

--- processor/bin/cimis-solar-clean-cron.sh
+++ processor/bin/cimis-solar-clean-cron.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grass -c ~/gdb/solar/$(date +%Y%m%d --date="yesterday") --exec make --directory=~/spatial-cimis/g.cimis/etc/ -f solar.mk clean-tmp $1
+grass -c ~/gdb/solar/$(date +%Y%m%d --date="last-week") --exec make --directory=~/spatial-cimis/g.cimis/etc/ -f solar.mk clean-tmp $1
