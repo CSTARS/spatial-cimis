626879e55d97c8184fb29bcb3797c7f576997e8f
George Scheer <gjscheer@ucdavis.edu>
2019-08-06 15:22:29 -0700

    utility updates

--- processor/bin/cimis-check-import-goes17.sh
+++ processor/bin/cimis-check-import-goes17.sh
@@ -3,4 +3,4 @@
 # check if today's bands were imported
 # if not run without -n option
 
-grass /home/cimis/gdb/goes17/cimis --exec g.cimis sec=goes cmd=import,solar arg="files='$(echo /home/cimis/CA/`date +%Y%m%d`T*-B2.pgm)'" -n
+grass /home/cimis/gdb/goes17/cimis --exec g.cimis sec=goes cmd=import,solar arg="files=$(echo /home/cimis/CA/`date +%Y%m%d`T*-B2.pgm)" -n
