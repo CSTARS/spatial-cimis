0d828bdf1d5103fc50a1f10dab2404191f5d161f
george_scheer <gjscheer@ucdavis.edu>
2020-06-08 14:53:03 -0700

    solar cleanup enhancements

--- processor/bin/cimis-solar-clean-cron.sh
+++ processor/bin/cimis-solar-clean-cron.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-grass -c ~/gdb/solar/$(date +%Y%m%d --date="last-week") --exec make --directory=~/spatial-cimis/g.cimis/etc/ -f solar.mk clean-tmp $1
+
+# clean the temp solar calculation data
+# leave essential data required to recalculate ETo
+
+#grass -c ~/gdb/solar/$(date +%Y%m%d --date="last-week") --exec make --directory=~/spatial-cimis/g.cimis/etc/ -f solar.mk clean-tmp $1
+
+cleanday ()
+{
+/usr/local/bin/grass -c ~/gdb/solar/$day --exec g.remove -f type=rast pattern=_hel*
+/usr/local/bin/grass -c ~/gdb/solar/$day --exec g.remove -f type=rast pattern=*????PST-G*
+/usr/local/bin/grass -c ~/gdb/solar/$day --exec g.remove -f type=rast pattern=*????PST-K*
+}
+
+if [[ $1 = [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9] ]];then
+	day=$1
+	cleanday
+elif [[ $1 = "" ]];then
+	day=$(date +%Y%m%d --date="last--week")
+	cleanday
+else
+  echo "Usage:  cimis-solar-clean-cron.sh [YYYYMMDD]"
+  echo ""
+  echo "Default is last week"
+fi
+
